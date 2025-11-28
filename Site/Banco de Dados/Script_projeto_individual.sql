a00c5364a50edd7b1c8f286cf9fa5be6befd1d7c
Enrico Assef <enrico.ferreira@sptech.school>
2025-11-28 20:52:32 -0300

    Login e Cadastros 100% funcionando e prontos, css da dashboard quase pronta (80%)

---
+++ Site/Banco de Dados/Script_projeto_individual.sql
@@ -0,0 +1,27 @@
+
+USE projeto_individual;
+
+CREATE TABLE quiz (
+id_quiz INT PRIMARY KEY, 
+nome_quiz VARCHAR(45)
+);
+
+CREATE TABLE usuario(
+id_usuario INT PRIMARY KEY AUTO_INCREMENT, 
+nome VARCHAR(45), 
+email VARCHAR(45), 
+senha VARCHAR(45)
+);
+
+CREATE TABLE questoes (
+id_questoes INT AUTO_INCREMENT, 
+resposta_certa INT, 
+resposta_errada INT,
+fk_idUsuario INT,
+fk_idQuestoes INT, 
+fk_idQuiz INT, 
+PRIMARY KEY (id_questoes, fk_idUsuario, fk_idQuiz), 
+FOREIGN KEY (fk_idUsuario) REFERENCES usuario(id_usuario),
+FOREIGN KEY (fk_idQuestoes) REFERENCES questoes(id_questoes),
+FOREIGN KEY (fk_idQuiz) REFERENCES quiz(id_quiz)
+);
